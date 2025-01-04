d5eb8d3c23dcf539eaaf8a9d2d03573ced2b2e3e
Ravi Seta <mr.raviseta@gmail.com>
2025-01-04 14:25:51 +0530

    initial commit

---
+++ HeartRateMeasure/HeartRateMeasure/HeartRateMeasureApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateMeasureApp.swift
+//  HeartRateMeasure
+//
+//  Created by Ravi Seta on 04/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateMeasureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
